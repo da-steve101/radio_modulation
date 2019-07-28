f7bed39b37f154bd32d7b014ab43fe927fccfaf4
Stephen Tridgell <stephen@tridgell.net>
2019-07-29 08:49:05 +1000

    Cleaned up TWN function, hopefully is faster now

--- train_tnn/quantization.py
+++ train_tnn/quantization.py
@@ -12,21 +12,12 @@ def shaped_relu( x, k = 1.0 ):
     return act  + tf.stop_gradient( quant - act )
 
 def trinarize( x, nu = 1.0 ):
-    clip_val = x # tf.clip_by_value( x, -1, 1 )
     x_shape = x.get_shape()
-    thres = nu * tf.reduce_mean(tf.abs(clip_val))
-    unmasked = tf.where(
-        tf.logical_and(
-            tf.greater( x, -thres ),
-            tf.less( x, thres )
-        ),
-        tf.constant( 0.0, shape = x_shape ),
-        clip_val )
+    thres = nu * tf.reduce_mean(tf.abs(x))
+    g_e = tf.cast( tf.greater_equal( x, thres ), tf.float32 )
+    l_e = tf.cast( tf.less_equal( x, -thres ), tf.float32 )
+    unmasked = tf.multiply( g_e + l_e, x )
     eta = tf.reduce_mean( tf.abs( unmasked ) )
-    t_x = tf.where( tf.less_equal( unmasked, -thres ),
-                    tf.multiply( tf.constant( -1.0, shape = x_shape ), eta ),
-                    unmasked )
-    t_x = tf.where( tf.greater_equal( unmasked, thres ),
-                    tf.multiply( tf.constant( 1.0, shape = x_shape ), eta ),
-                    t_x )
+    t_x = tf.multiply( l_e, -eta )
+    t_x = t_x + tf.multiply( g_e, eta )
     return x + tf.stop_gradient( t_x - x )
