184f5421a7b4a15205d45a938dd30c8f2fe0fd78
Stephen Tridgell <stephen@tridgell.net>
2019-07-28 16:16:18 +1000

    Cleaned up some generate scripts

--- train_tnn/train_all_models.sh
+++ train_tnn/train_all_models.sh
@@ -6,61 +6,40 @@ LR=0.001
 BATCH_SIZE=128
 MODEL_DIR=/home/stephen/radio_modulation/models
 DATA_DIR=/opt/datasets/deepsig
+TRAIN_ARGS="--steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd --teacher_dset"
 # make resnet full precision
 python3 run_cnn.py --model_name $MODEL_DIR/resnet --dataset $DATA_DIR/modulation_classification_train.rcrd \
 --steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --resnet --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd
 # run resnet through the training set to add student teacher
-python3 add_resnet_to_train.py --dataset_rcrd /opt/datasets/deepsig/modulation_classification_train.rcrd --rcrd_prefix modulation_classification \
---rcrd_suffix .rcrd --resnet_model $MODEL_DIR/resnet --gpus $GPUS
+if [ ! -e $DATA_DIR/modulation_classification_resnet_train.rcrd  ]
+then
+  echo "Teacher Dataset does not exist. Creating ... "
+  python3 add_resnet_to_train.py --dataset_rcrd /opt/datasets/deepsig/modulation_classification_train.rcrd --rcrd_prefix modulation_classification \
+  --rcrd_suffix .rcrd --resnet_model $MODEL_DIR/resnet --gpus $GPUS
+fi
 # make vgg full precision
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_fp_64 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --full_prec --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---no_filt_vgg 64 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_fp_64 $TRAIN_ARGS --full_prec --no_filt_vgg 64
 # vgg fp with dense 512
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_fp_64_d512 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --full_prec --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---no_filt_vgg 64,64,64,64,64,64,64,512,512,24 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_fp_64_d512 $TRAIN_ARGS --full_prec --no_filts 64,64,64,64,64,64,64,512,512,24
 # vgg fp 128
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_fp_128 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --full_prec --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---no_filt_vgg 128,128,128,128,128,128,128,512,512,24 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_fp_128 $TRAIN_ARGS --full_prec --no_filts 128,128,128,128,128,128,128,512,512,24
 #custom Vgg
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_fp_3x64_3x128_512_512_24 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --full_prec --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---no_filts 64,64,64,128,128,128,128,512,512,24 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_fp_3x64_3x128_512_512_24 $TRAIN_ARGS --full_prec --no_filts 64,64,64,128,128,128,128,512,512,24
 # make vgg TWN with 64
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_f64 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --twn --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---nu_conv 1.2 --nu_dense 0.7 --no_filt_vgg 64,64,64,64,64,64,64,512,512,24 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_f64 $TRAIN_ARGS --twn --nu_conv 1.2 --nu_dense 0.7 --no_filts 64,64,64,64,64,64,64,512,512,24
 # make vgg TWN with 96
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_f96 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --twn --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---nu_conv 1.2 --nu_dense 0.7 --no_filt_vgg 96,96,96,96,96,96,96,512,512,24 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_f96 $TRAIN_ARGS --twn --nu_conv 1.2 --nu_dense 0.7 --no_filts 96,96,96,96,96,96,96,512,512,24
 # make vgg TWN with 128
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_f128 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --twn --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---nu_conv 1.2 --nu_dense 0.7 --no_filt_vgg 128,128,128,128,128,128,128,512,512,24 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_f128 $TRAIN_ARGS --twn --nu_conv 1.2 --nu_dense 0.7 --no_filts 128,128,128,128,128,128,128,512,512,24
 #custom Vgg
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_3x64_3x128_512_512_24 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --twn --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---nu_conv 1.2 --nu_dense 0.7 --no_filts 64,64,64,128,128,128,128,512,512,24 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_3x64_3x128_512_512_24 $TRAIN_ARGS --twn --nu_conv 1.2 --nu_dense 0.7 --no_filts 64,64,64,128,128,128,128,512,512,24
 # Vgg bin 64
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_bin_64 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --twn_binary_act --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---nu_conv 1.2 --nu_dense 0.7 --no_filt_vgg 64 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_bin_64 $TRAIN_ARGS --twn_binary_act --nu_conv 1.2 --nu_dense 0.7 --no_filts 64
 # Vgg bin 64 dense 512
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_bin_64_d512 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --twn_binary_act --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---nu_conv 1.2 --nu_dense 0.7 --no_filt_vgg 64,64,64,64,64,64,64,512,512,24 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_bin_64_d512 $TRAIN_ARGS --twn_binary_act --nu_conv 1.2 --nu_dense 0.7 --no_filts 64,64,64,64,64,64,64,512,512,24
 # Vgg bin 128
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_bin_128 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --twn_binary_act --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---nu_conv 1.2 --nu_dense 0.7 --no_filt_vgg 128,128,128,128,128,128,128,512,512,24 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_bin_128 $TRAIN_ARGS --twn_binary_act --nu_conv 1.2 --nu_dense 0.7 --no_filts 128,128,128,128,128,128,128,512,512,24
 # Vgg inrc Act
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_incr1_128 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --twn_incr_act 1 --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---nu_conv 1.2 --nu_dense 0.7 --no_filt_vgg 128,128,128,128,128,128,128,512,512,24 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_incr1_128 $TRAIN_ARGS --twn_incr_act 1 --nu_conv 1.2 --nu_dense 0.7 --no_filts 128,128,128,128,128,128,128,512,512,24
 # Vgg inrc Act 2
-python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_incr2_128 --dataset $DATA_DIR/modulation_classification_resnet_train.rcrd \
---steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --twn_incr_act 2 --gpus $GPUS --val_dataset $DATA_DIR/modulation_classification_test_snr_30.rcrd \
---nu_conv 1.2 --nu_dense 0.7 --no_filt_vgg 128,128,128,128,128,128,128,512,512,24 --teacher_dset
+python3 run_cnn.py --model_name $MODEL_DIR/vgg_twn_nu_1.2_0.7_incr2_128 $TRAIN_ARGS --twn_incr_act 2 --nu_conv 1.2 --nu_dense 0.7 --no_filts 128,128,128,128,128,128,128,512,512,24
--- verilog_generation/generate_tw_vgg10.py
+++ verilog_generation/generate_tw_vgg10.py
@@ -3,16 +3,18 @@
 import twn_generator as twn
 import csv
 import sys
+import os
 
 def make_conv( idx, bw, create_op, model_dir ):
     f_in = model_dir + "/vgg_conv_lyr%d.csv" % idx
     f_tree = model_dir + "/vgg_conv_lyr%d_td_cse.csv" % idx
     mod_out = "conv%d" % idx
     f_out = model_dir + "/" + mod_out + ".sv"
-    matrix, no_in, no_out, initial_no_adds = twn.get_matrix( f_in  )
-    matrix = twn.td_CSE( matrix )
-    twn.write_output( f_tree, matrix, initial_no_adds, no_in, no_out )
-    twn.verify_tree( f_in, f_tree )
+    if not os.path.exists(f_tree):
+        matrix, no_in, no_out, initial_no_adds = twn.get_matrix( f_in  )
+        matrix = twn.td_CSE( matrix )
+        twn.write_output( f_tree, matrix, initial_no_adds, no_in, no_out )
+        twn.verify_tree( f_in, f_tree )
     f = open( f_out, "w" )
     f.write( twn.SMM_generate( f_tree, mod_out, bw, bw, create_op ) )
     f.close()
