638f3bf53707adbe1bb2d92854c05467aea45d07
Stephen Tridgell <stephen@tridgell.net>
2019-08-09 11:36:13 +1000

    Need to distinguish dense from conv BN lyr

--- verilog_generation/generate_bin128.sh
+++ verilog_generation/generate_bin128.sh
@@ -17,9 +17,9 @@ do
 python3 generate_bn_vecs.py --file_in $MODEL_DIR/vgg_bn_lyr$i"_a_b.csv" --file_out $MODEL_DIR/bn$i.sv --bn_id $i --rshift $BN_PREC --bw_in 16 --bw_out 1 --maxval 1
 done
 
-python3 generate_bn_vecs.py --file_in $MODEL_DIR/vgg_bn_dense_1_a_b.csv --file_out $MODEL_DIR/bnd1.sv --bn_id 1 --rshift $BN_PREC --bw_in 16 --bw_out 1 --maxval 1
+python3 generate_bn_vecs.py --file_in $MODEL_DIR/vgg_bn_dense_1_a_b.csv --file_out $MODEL_DIR/bnd1.sv --bn_id D1 --rshift $BN_PREC --bw_in 16 --bw_out 1 --maxval 1
 python3 generate_dense_vecs.py --file_in $MODEL_DIR/vgg_dense_1.csv --file_out $MODEL_DIR/dense_1.sv --lyr 1 --bw_w 2 --tput 2
-python3 generate_bn_vecs.py --file_in $MODEL_DIR/vgg_bn_dense_2_a_b.csv --file_out $MODEL_DIR/bnd2.sv --bn_id 2 --rshift $BN_PREC --bw_in 16 --bw_out 1 --maxval 1
+python3 generate_bn_vecs.py --file_in $MODEL_DIR/vgg_bn_dense_2_a_b.csv --file_out $MODEL_DIR/bnd2.sv --bn_id D2 --rshift $BN_PREC --bw_in 16 --bw_out 1 --maxval 1
 python3 generate_dense_vecs.py --file_in $MODEL_DIR/vgg_dense_2.csv --file_out $MODEL_DIR/dense_2.sv --lyr 2 --bw_w 2
 python3 generate_dense_vecs.py --file_in $MODEL_DIR/vgg_dense_3.csv --file_out $MODEL_DIR/dense_3.sv --lyr 3 --rshift $PREC --bw_w 16
 
